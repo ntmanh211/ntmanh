c86e5aac7f72dec9b5b41c01fa5e2e3414d5ade5
ntmanh2111998 <ntmanh2111998@gmail.com>
2019-05-15 20:50:55 +0700

    phien ban 1

--- M6.PHP/day1/funstring.php
+++ M6.PHP/day1/funstring.php
@@ -10,13 +10,23 @@
     <li>strlen() là hàm đến số thứ tự trong chuỗi</li>
     <li>str_word_count() là hàm đếm số từ trong 1 chuỗi</li>
     <li>strpos() tìm kiếm 1 chuỗi con trong 1 chuỗi mẹ và trả về false nếu không tìm được,
-    trả về vị trí bắt đầu của chuỗi con trong chuỗi mẹ</li>
-    <li>str_replace() thay thế chuỗi 1 thành chuỗi 2 trong 1 chuỗi mẹ</li>
+    trả về vị trí bắt đầu của chuỗi con trong chuỗi mẹ
+        strpos("Chuỗi mẹ","Chuỗi cần tìm")
+    </li>
+    <li>str_replace() thay thế chuỗi 1 thành chuỗi 2 trong 1 chuỗi mẹ
+        str_replace("Chuỗi cần tìm", "Chuỗi thay thế", "Chuỗi mẹ")
+    </li>
 </ol>
 <?php
     $str = "Nguyễn Thị Minh Ánh";
     echo "Độ dài chuỗi: ".strlen($str);
     echo "<br>Số từ trong chuỗi:".str_word_count($str);
+    echo "<br>";
+    var_dump(strpos("Nguyễn Thị Minh Ánh", "Ánh"));
+    echo "<br>";
+    var_dump(strpos("Nguyễn Thị Minh Ánh", "Quân"));
+    echo "<br>";
+    echo str_replace("Ánh", "Quân", "Nguyễn Minh Ánh");
 ?>
 </body>
 </html>
