f4d60bc6f9f0ffac6ea205ac999bf5b86ed58a0b
ntmanh2111998 <ntmanh2111998@gmail.com>
2019-05-22 20:45:14 +0700

    phien ban 1

---
+++ M6.PHP/day 2/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+</head>
+<body>
+<h1>Câu lệnh điều kiện trong PHP</h1>
+<?php
+    $age = 19;
+    if ($age < 18){
+        echo "Bạn chưa trưởng thành";
+    }
+    else {
+        echo "Bạn đã trưởng thành";
+    }
+?>
+</body>
+</html>
