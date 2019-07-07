b6d9afe43520548aa3060ada86b862e2dbea72a4
ntmanh2111998 <ntmanh2111998@gmail.com>
2019-07-07 20:35:25 +0700

    phien ban 1

---
+++ M12.Laravel/blog1/resources/views/simpleadmin/permission/permission.blade.php
@@ -0,0 +1,13 @@
+@extends("simpleadmin.layouts.simpleadmin")
+
+@section("title")
+    Phân quyền
+@endsection
+
+@section("content")
+    <div id="page-wrapper" >
+        <div id="page-inner">
+            Nội dung phần phân quyền
+        </div>
+    </div>
+@endsection
---
+++ M12.Laravel/blog1/routes/web.php
@@ -0,0 +1,50 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('simpleadmin.homepage.homepage1');
+});
+
+Route::get('/category', function (){
+    return view('simpleadmin.category.category');
+});
+Route::get('/article', function (){
+    return view('simpleadmin.article.article');
+});
+Route::get('/banner', function (){
+    return view('simpleadmin.banner.banner');
+});
+Route::get('/email', function (){
+    return view('simpleadmin.email.email');
+});
+Route::get('/permission', function (){
+    return view('simpleadmin.permission.permission');
+});
+Route::get('/product', function (){
+    return view('simpleadmin.product.product');
+});
+Route::get('/user', function (){
+    return view('simpleadmin.user.user');
+});
+
+
+Route::get('/abc', function (){
+    echo "ABC";
+});
+Route::get('/def', function (){
+    return view('def');
+});
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
